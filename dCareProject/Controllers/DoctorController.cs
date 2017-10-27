4e714bfbb97033c9c8bcefa6878271de20f89f32
rioyclai <31389661+rioyclai@users.noreply.github.com>
2017-10-28 01:51:26 +0800

    update Doctor Controller
    20171028.
    Updated Doctor controller and modified the CSS/JS construction.

---
+++ dCareProject/Controllers/DoctorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dCareProject.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Login(string username ,string password)
+        //{
+        //    return RedirectToAction("Index","Doctor");
+        //}
+    }
+}
