11763dc8f29cfdba53cb238fa9910d9c15f4cb00
chungyz <33109499+chungyz@users.noreply.github.com>
2017-10-27 16:12:22 +0800

    Update font ,del index (#4)
    * Update patient view 10/27
    * Update font,index del 10/27
    
    0.2

---
+++ dCareProject/Controllers/PatientController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dCareProject.Controllers {
+    public class PatientController : Controller {
+        // GET: Patient
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult step1() {
+            return View();
+        }
+        public ActionResult step2() {
+            return View();
+        }
+
+        public ActionResult step3() {
+            return View();
+        }
+        public ActionResult look() {
+            return View();
+        }
+    }
+}
+
+
+    
+
