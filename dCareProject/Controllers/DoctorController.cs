9a33b3a08f6a27b05860a8909004c0ad09bb2d19
tsuitingwei <tsuitingwei@gamil.com>
2017-10-27 14:42:10 +0800

    1027 add
    add DoctorController
    add View Login
    add View Index
    add Folder css,js,img

---
+++ dCareProject/Controllers/DoctorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dCareProject.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Login(string username ,string password)
+        //{
+        //    return RedirectToAction("Index","Doctor");
+        //}
+    }
+}
