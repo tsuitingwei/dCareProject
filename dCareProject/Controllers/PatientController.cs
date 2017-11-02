f55ddd3f7d49c4fe6ba35137b26e9e402f2150ce
rioyclai <31389661+rioyclai@users.noreply.github.com>
2017-11-02 12:05:28 +0800

    updated / 20171102 1205 _ Chungyz master (#10)
    * update paitetlogin,seeadoctor controller ,loginX2,patientreserviation,look1,patientview view
    
    11/01
    
    * update 10/02  new seeadoctor,patientlogin
    
    10/02

--- dCareProject/Controllers/PatientController.cs
+++ dCareProject/Controllers/PatientController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using dCareProject.Models;
 
 namespace dCareProject.Controllers {
     public class PatientController : Controller {
+        private dCareEntities db = new dCareEntities();
         // GET: Patient
         public ActionResult Index() {
             return View();
@@ -17,16 +19,37 @@ namespace dCareProject.Controllers {
         public ActionResult step2() {
             return View();
         }
+        public ActionResult step4() {
+            return View();
+        }
 
         public ActionResult step3() {
-            return View();
+            var randon = (from o in db.醫生
+                         orderby Guid.NewGuid()
+                         select o).Take(1);
+
+            return View(randon.ToList());
         }
         public ActionResult look() {
-            return View();
+            var query = from o in db.預約表
+                        join p in db.醫生 on o.醫生ID equals p.ID
+                        where o.病人ID == 8
+                        select new patient {
+                            section = p.科別,
+                            date = o.登記時間.Value,
+                            doctor = p.姓名
+                                                    };
+                        ViewBag.name = query.ToList();
+                        return View();
         }
+        
+         public ActionResult look1() {
+            var query = from o in db.醫生
 
-        public ActionResult look1() {
-            return View();
+
+                         select o;
+
+                   return View(query.ToList());
         }
 
         public ActionResult patientview() {
