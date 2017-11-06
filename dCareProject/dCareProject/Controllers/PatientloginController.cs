06fe990c65e95d45573662b6d5252167178e17bb
chungyz <33109499+chungyz@users.noreply.github.com>
2017-11-06 13:54:40 +0800

    update SQL connection
    11/06

---
+++ dCareProject/dCareProject/Controllers/PatientloginController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dCareProject.Models;
+
+namespace dCareProject.Controllers
+{
+    public class PatientloginController : Controller
+    {
+        private dCareEntities db = new dCareEntities();
+        // GET: Patientlogin
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult patientreservation() {
+            var query = from o in db.預約表
+                        join p in db.醫生 on o.醫生ID equals p.ID
+                        join c in db.病人 on o.病人ID equals c.ID
+                        join d in db.看診紀錄表 on o.看診紀錄表ID equals d.ID
+                        where c.ID == 3
+
+                        select new patient {
+                            section = p.科別,
+                            date = o.登記時間.Value,
+                            doctor = p.姓名,
+                            name = c.姓名,
+                            gender = c.性別,
+                            number = c.電話,
+                            address = c.地址,
+                            birthday = c.出生年月日,
+                            addressID = c.看診點ID.Value,
+                            healthDate = d.健檢時間,
+                            patientid = o.病人ID.Value
+                            
+                           
+
+                        };
+            ViewBag.name = query.ToList();
+ 
+            return View();
+            
+    }
+        public ActionResult look1() {
+            var query = from o in db.醫生
+
+
+                        select o;
+
+            return View(query.ToList());
+        }
+    }
+}
---
+++ dCareProject/dCareProject/Models/listWaitView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dCareProject.Models {
+    public class listWaitView {
+        public int id;
+        public string name;
+        public DateTime time;
+        public string gender;
+        public string phone;
+        public string address;
+        public DateTime birth;
+        public int pid;
+    }
+}
